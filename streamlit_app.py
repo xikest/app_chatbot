2c09ff5953d3541aa770035e6e2a9a6c6077ad34
xikest <xikest12@gmail.com>
2023-10-30 22:45:51 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,16 +15,14 @@ def speech2text(audio):
     wav_file.write(audio.tobytes())
     wav_file.close()
 
-    # 음원 파일 열기
+    # open voice
     audio_file = open(filename, "rb")
-    # Whisper 모델을 활용해 텍스트 얻기
+    # get text by Whisper
     transcript = openai.Audio.transcribe("whisper-1", audio_file)
     audio_file.close()
-    # 파일 삭제
+    # del file
     os.remove(filename)
     return transcript["text"]
-
-
 def ask_gpt(prompt, model):
     response = openai.ChatCompletion.create(model=model,
                                             messages=prompt)
@@ -49,7 +47,7 @@ def text2speech(response):
     # 파일 삭제
     os.remove(filename)
 
-##### 메인 함수 #####
+
 def main():
     # 기본 설정
     st.set_page_config(
@@ -58,7 +56,7 @@ def main():
 
     flag_start = False
 
-    # session state 초기화
+    # session state initialize
     if "chat" not in st.session_state:
         st.session_state["chat"] = []
 
@@ -72,15 +70,15 @@ def main():
     # if "check_audio_uploaded" not in st.session_state:
     #     st.session_state["check_audio_uploaded"] = []
 
-    # 음성 파일 업로드
+    # upload voice file
     # uploaded_audio = st.file_uploader("Upload an audio file (MP3 or WAV)", type=["mp3", "wav"])
 
-    # 제목
+    # title
     st.header("Your Voice App")
-    # 구분선
+    # segment
     st.markdown("---")
 
-    # 기본 설명
+    # basic descr
     with st.expander("What is this APP", expanded=True):
         st.write(
             """     
@@ -94,11 +92,11 @@ def main():
 
 
 
-    # 사이드바 생성
+    # side bar
     with st.sidebar:
         openai.api_key = st.text_input(label="OPENAI API Key", placeholder="Enter Your API Key", type="password")
         model = st.radio(label="GPT Model", options=["gpt-3.5-turbo", "gpt-4"])
-        mode = st.radio(label="Mode", options=["Ask a Question", "Translate"])
+        mode = st.radio(label="Mode", options=["Translate", "Ask a Question"])
 
     col1, col2 = st.columns(2)
     with col1:
@@ -118,17 +116,17 @@ def main():
 
     with col2:
 
-        st.subheader("답변")
+        st.subheader("Answer")
         if flag_start:
             response = ask_gpt(st.session_state["messages"], model)
 
             st.session_state["messages"] = st.session_state["messages"] + [{"role": "system", "content": response}]
 
-            # 채팅 시각화를 위한 답변 내용 저장
+            # ready to chat
             now = datetime.now().strftime("%H:%M")
             st.session_state["chat"] = st.session_state["chat"] + [("bot", now, response)]
 
-            # 채팅 형식으로 시각화 하기
+            # show chat
             for sender, time, message in st.session_state["chat"]:
                 if sender == "user":
                     st.write(
@@ -141,7 +139,7 @@ def main():
                         unsafe_allow_html=True)
                     st.write("")
 
-            # gTTS 를 활용하여 음성 파일 생성 및 재생
+            # speak
             # text2speech(response)
 
 
