cf266783f57e54bcdc26551f0f38399492dc1438
xikest <xikest12@gmail.com>
2023-11-07 01:58:34 +0900

    Update botmanger.py

--- functions/botmanger.py
+++ functions/botmanger.py
@@ -20,32 +20,34 @@ class BotManager:
         self.app = Application.builder().token(self.token).build()
         self.aim = AIManager(self.api_key)
         self.log_manager = None
-        self.logfile:str = ""
+        self.bot_log:str = None
 
     def run(self):
         self.add_handlers()
         self.app.run_polling(allowed_updates=Update.ALL_TYPES)
 
     def add_handlers(self):
-        self.app.add_handler(CommandHandler("new", self.newbot_command))
+        self.app.add_handler(CommandHandler("hey", self.newbot_command))
         self.app.add_handler(CommandHandler("img", self.img_command))
         self.app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.chatgpt))
 
     async def chatgpt(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Echo the user message."""
         user_message = update.message.text
-        user_id = update.message.chat_id
-        if not self.log_manager:
-            self.logfile = await self.newbot(user_id)
-            self.log_manager = LogManager()
-            self.log_manager.load_log(self.logfile)
-        self.log_manager.add_message("user", user_message)
-        prompt = self.log_manager.messages_prompt
-        print(f"user: {prompt[-1].get('content')}")
-        bot_response = self.aim.get_text_from_gpt(prompt)
-        print(f"bot: {bot_response}")
-        self.log_manager.add_message("assistant", bot_response)
-        self.log_manager.save_log(self.logfile)
+        try:
+            if self.bot_log:
+                self.log_manager = LogManager()
+                self.log_manager.load_log(self.bot_log)
+
+            self.log_manager.add_message("user", user_message)
+            prompt = self.log_manager.messages_prompt
+            print(f"user: {prompt[-1].get('content')}")
+            bot_response = self.aim.get_text_from_gpt(prompt)
+            print(f"bot: {bot_response}")
+            self.log_manager.add_message("assistant", bot_response)
+            self.log_manager.save_log(self.bot_log)
+        except Exception as e:
+            bot_response = user_message
         await update.message.reply_text(bot_response)
 
     async def img_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -56,7 +58,7 @@ class BotManager:
 
     async def newbot_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_id = update.message.chat_id
-        self.logfile = await self.newbot(user_id)
+        self.bot_log = await self.newbot(user_id)
 
     async def newbot(self, user_id) -> str:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
