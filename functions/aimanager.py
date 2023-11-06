3c25c656d81a29ee8a7e125800d7aa8f7027ac5c
xikest <xikest12@gmail.com>
2023-11-07 02:43:01 +0900

    .

--- functions/aimanager.py
+++ functions/aimanager.py
@@ -1,20 +1,19 @@
-import openai
+from openai import OpenAI
 
 class AIManager:
     def __init__(self, api_key):
-        self.API_KEY = api_key
-        openai.api_key = self.API_KEY
+        self.client = OpenAI(api_key=api_key)
         self.messages_prompt = []
 
     def add_message_to_prompt(self, role, content):
         self.messages_prompt.append({"role": role, "content": content})
 
     def get_text_from_gpt(self, prompt):
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt)
-        system_message = response["choices"][0]["message"]
-        answer = system_message["content"]
+        response = self.client.chat.completions.create(model="gpt-3.5-turbo", messages=prompt)
+        answer = response.choices[0].message.content
         return answer
     def getImageURLFromDALLE(self, user_input):
-        response = openai.Image.create(prompt=user_input,n=1,size="512x512")
-        image_url = response['data'][0]['url']
+        response = self.client.images.generate(prompt=user_input,n=1,size="512x512")
+        image_url = response.data[0].url
         return image_url
+
