e5befaa16fd9fba90618b32ce931d9b2588f006c
xikest <xikest12@gmail.com>
2023-11-07 10:54:59 +0900

    Update botmanger.py

--- functions/botmanger.py
+++ functions/botmanger.py
@@ -54,24 +54,19 @@ class BotManager:
             self.log_manager.save_log(self.bot_log)
 
 
-        except TimeoutError as tr_e:
-            print(f"time out error: {tr_e}")
+        except TimeoutError as e:
+
+
+            await self.handle_error(e, type='time out')
+
             user_id = update.message.chat_id
             await self.newbot(user_id)
-
-            error_message = str(tr_e)
-            with open("err/error_t.txt", "w") as error_file:
-                error_file.write(error_message)
             pass
 
         except Exception as e:
-            error_message = str(e)
-            with open("err/error.txt", "w") as error_file:
-                error_file.write(error_message)
+            await self.handle_error(e, type='normal')
 
         await update.message.reply_text(bot_response)
-
-
 
     async def img_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_message = update.message.text
@@ -87,3 +82,13 @@ class BotManager:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
         self.bot_log =  f"log_{user_id}_{current_time}"
         return None
+
+    async def handle_error(self, error, type, update) -> None:
+        error_message = f"{type} error: {error}"
+        print(error_message)
+
+        with open(f"err/{type} error.txt", "w") as error_file:
+            error_file.write(error_message)
+        await update.message.reply_text(error_message)
+        return None
+
