b3a1ea639daa2fc3a56f86ae5fd2fb4365208e5f
xikest <xikest12@gmail.com>
2023-11-07 10:19:38 +0900

    .

--- functions/botmanger.py
+++ functions/botmanger.py
@@ -1,4 +1,5 @@
 import logging
+import asyncio
 from datetime import datetime
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
@@ -20,7 +21,7 @@ class BotManager:
         self.app = Application.builder().token(self.token).build()
         self.aim = AIManager(self.api_key)
         self.log_manager = None
-        self.bot_log:str = None
+        self.bot_log = None
 
     def run(self):
         self.add_handlers()
@@ -34,21 +35,43 @@ class BotManager:
     async def chatgpt(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Echo the user message."""
         user_message = update.message.text
+        bot_response = user_message
+
         try:
             if self.bot_log:
                 self.log_manager = LogManager()
                 self.log_manager.load_log(self.bot_log)
 
             self.log_manager.add_message("user", user_message)
-            prompt = self.log_manager.messages_prompt
+            prompt_first= self.log_manager.messages_prompt.pop(0)
+            prompt_recent = self.log_manager.messages_prompt[-5:]  # 최근 5개만
+            prompt = [prompt_first] + prompt_recent
+            print(f"prompt: {prompt}")
             print(f"user: {prompt[-1].get('content')}")
             bot_response = self.aim.get_text_from_gpt(prompt)
             print(f"bot: {bot_response}")
             self.log_manager.add_message("assistant", bot_response)
             self.log_manager.save_log(self.bot_log)
+
+
+        except TimeoutError as tr_e:
+            print(f"time out error: {tr_e}")
+            user_id = update.message.chat_id
+            await self.newbot(user_id)
+
+            error_message = str(tr_e)
+            with open("error_t.txt", "w") as error_file:
+                error_file.write(error_message)
+            pass
+
         except Exception as e:
-            bot_response = user_message
+            error_message = str(e)
+            with open("error.txt", "w") as error_file:
+                error_file.write(error_message)
+
         await update.message.reply_text(bot_response)
+
+
 
     async def img_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_message = update.message.text
@@ -58,8 +81,9 @@ class BotManager:
 
     async def newbot_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_id = update.message.chat_id
-        self.bot_log = await self.newbot(user_id)
+        await self.newbot(user_id)
 
-    async def newbot(self, user_id) -> str:
+    async def newbot(self, user_id) -> None:
         current_time = datetime.now().strftime("%Y%m%d%H%M%S")
-        return f"log_{user_id}_{current_time}"
+        self.bot_log =  f"log_{user_id}_{current_time}"
+        return None
