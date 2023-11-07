e842ccb65518d35f7c3615bda210cc847340d304
xikest <xikest12@gmail.com>
2023-11-07 16:57:19 +0900

    Update botmanger.py

--- functions/botmanger.py
+++ functions/botmanger.py
@@ -49,7 +49,7 @@ class BotManager:
                 bot_response = self.aim.get_text_from_gpt(prompt)
                 end_time = time.time()  # 함수 종료 시간 기록
                 execution_time = end_time - start_time  # 실행 시간 계산
-                print(f"bot: {bot_response}|({round(execution_time,1)}s")
+                print(f"bot: {bot_response}|({round(execution_time,1)}s)")
                 self.log_manager.add_message("assistant", bot_response)
                 self.log_manager.save_log(self.bot_log)
             else:
